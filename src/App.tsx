c93b8155be017dd89a340444527401ab7c9d127f
React-Props <daniel.linker@intellicode-solutions.de>
2024-08-12 15:33:18 +0200

    structure created

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import {useState} from 'react'
+
 import './App.css'
+import {TodoList} from "./components/TodoList.tsx";
+import {Todo} from "./types/todo.ts";
 
 function App() {
 
+    const todo: Todo[] = [
+        {id: 1, text: "Learn React", completed: false},
+        {id: 2, text: "Learn TypeScript", completed: false},
+        {id: 3, text: "Learn Vite", completed: false},
+    ]
+
     return (
         <>
-            <h1>Test</h1>
+            <h1>Todo</h1>
+            <TodoList todo={todo}/>
+            <todoItem/>
         </>
     )
 }
---
+++ src/components/TodoItem.tsx
@@ -0,0 +1,9 @@
+
+
+export const TodoItem = () => {
+    return (
+        <div>
+            <H1>Todoitem</H1>
+        </div>
+    )
+}
